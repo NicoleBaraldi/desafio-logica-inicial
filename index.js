6581388ff6a19b6b5cb1ed8f37718a359ec7df20
NicoleBaraldi <nikk.wolff2@gmail.com>
2026-01-22 17:13:42 -0300

    Resolução

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log ("Hello Word")
+let nome = "Nikk"
+let quantidadeDeExperiência = 5010
+let nivel = ""
+
+switch (true) {
+    case quantidadeDeExperiência < 1000:
+        nivel = "Ferro"
+        break;
+
+    case quantidadeDeExperiência <= 2000:
+        nivel = "Bronze"
+        break;
+
+    case quantidadeDeExperiência <= 5000:
+        nivel = "Prata"
+        break;
+
+    case quantidadeDeExperiência <= 7000:
+        nivel = "Ouro"
+        break;
+
+    case quantidadeDeExperiência <= 8000:
+        nivel = "Platina"
+        break;
+
+    case quantidadeDeExperiência <= 9000:
+        nivel = "Ascendente"
+        break;
+
+    case quantidadeDeExperiência <= 10000:
+        nivel = "Imortal"
+        break;
+
+    default:
+        nivel = "Radiante"
+}
+console.log("O Herói de Nome: " + nome + " está no nível de: " + nivel)
